3f3dfa827f18db79d44ca60c9d94609c061007fd
nika2004 <nika.imamberdieva22@sitechhs.com>
2022-02-07 20:39:22 -0500

    day 2

---
+++ Controllers/shopController.js
@@ -0,0 +1,31 @@
+exports.middlewareSample = (req, res, next) => {
+    req.name = "TEST";
+    next();
+};
+
+
+exports.homePage = async (req, res) => {
+    const stores = ["Dunkin", "Tim Hortons", "Starbucks"];
+    try {
+      console.log(req.name);
+      res.json(stores);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  exports.authMiddleware = async (req, res, next) => {
+      if(req.body.user){
+          next();
+      } else {
+          res.json("You must be signed in")
+      }
+  };
+
+  exports.authPage = async (req,res) => {
+      try {
+          res.json(req.body.user)
+      } catch (error) {
+          console.log(error);
+      }
+  }
